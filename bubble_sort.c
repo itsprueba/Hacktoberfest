44a804cf50224e3ccc2148eb590fc0c77d299d6b
Ayush Srivastava <55127997+entrymaster@users.noreply.github.com>
2019-10-16 21:47:22 +0530

    created bubble_sort.c
    this program takes number from user stores in an array and then arrange the number in ascending order.

---
+++ bubble_sort.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+void bubble(int ar[]);
+
+int main(void)
+
+{
+	
+int a[10],i;
+for(i=0;i<10;i++)
+
+scanf("%d",&a[i]);
+
+bubble(a);
+	
+for(i=0;i<10;i++)
+	
+printf("%d\t",a[i]);
+
+}
+
+void bubble(int ar[])
+
+{
+	
+int i,j,c;
+	
+for(i=0;i<9;i++)
+	
+{
+		
+for(j=0;j<9-i;j++)
+
+if(ar[j]>ar[j+1])
+	
+{
+			
+c=ar[j];
+
+ar[j]=ar[j+1];
+
+ar[j+1]=c;
+
+}
+	
+}
+}

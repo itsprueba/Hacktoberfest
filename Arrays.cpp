01d82b79d53c6f98e13ab77d8b7216454b78c612
Zeeshan <66550180+ZeeshanK07@users.noreply.github.com>
2022-10-11 23:59:51 +0530

    Updated Arrays along with user controlls
    Addded the option to print the array normally as well as reverse array also provided the user input so that the user can print the array as their requirement.

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,19 +1,47 @@
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-    int a;
+    int a,requirement;
+
+    cout<<"How large you want to create your array"<<endl;
     cin>>a;
+
+    cout<<"Enter 1 if you want to print the reverse array"<<endl;
+    cout<<"Enter any number if you want to print the array normally"<<endl;
+    cin>>requirement;
+
+    if (requirement==1)
+    {
+    cout<<"You have selected to print the reverse array"<<endl;
+    }
+    else{cout<<"You have selected to print the array normally"<<endl;}
+    
+
     int array[a];
-    for(int i=0;i<a;i++)
+
+    for (int i = 0; i < a; i++)
     {
-        cin>>array[i];
+        cout<<"Enter the "<<i+1<<" value of the array"<<endl;
+        cin >> array[i];
     }
-    for(int i=0;i<a;i++)
+
+    if (requirement==1)
     {
-        cout<<array[a-i-1]<<" ";
+        for (int i = 0; i < a; i++)
+        {
+            cout << array[a - i - 1] << " ";
+        }
     }
+    else{
+        for (int i = 0; i < a; i++)
+        {
+            cout << array[i] << " ";
+        }
+        
+    }
+    
     return 0;
 }

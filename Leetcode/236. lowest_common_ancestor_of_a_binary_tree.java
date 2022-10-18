ad3d77ee6e5c97cd12d06ea5c8b9bcd6075f1a0a
Chavi Abrol <64221944+chaviabrol@users.noreply.github.com>
2022-10-18 17:00:32 +0530

    Create 236. lowest_common_ancestor_of_a_binary_tree.java

---
+++ Leetcode/236. lowest_common_ancestor_of_a_binary_tree.java
@@ -0,0 +1,15 @@
+ public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root.val == p.val || root.val == q.val){
+            
+            return root;
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        
+        if(left == null ) return right;
+        if(right == null) return left;
+        
+        return root;
+ }
